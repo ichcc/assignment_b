3b36d34a6cfc0dc44e9991f67e4ed1d417b7b271
Volodymyr Brodskyi <ichcc@me.com>
2023-07-22 20:10:49 +0300

    Removed ipv6

--- terraform/01_vpc.tf
+++ terraform/01_vpc.tf
@@ -3,16 +3,11 @@ module "vpc" {
 
   name                                          = "bizzabo"
   cidr                                          = "10.0.0.0/16"
-  enable_ipv6                                   = true
-  public_subnet_assign_ipv6_address_on_creation = true
+
 
   azs             = ["${local.region}a", "${local.region}b"]
-  private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-
-  public_subnet_ipv6_prefixes  = [0, 1]
-  private_subnet_ipv6_native   = true
-  private_subnet_ipv6_prefixes = [2, 3]
 
 
   enable_nat_gateway     = true
